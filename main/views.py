1885fe66893bd7e983681c44251ef2ae8d72b2c4
harshdew02 <harshdewangan032@gmail.com>
2022-11-27 14:21:34 +0530

    Updated all files

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request , 'main/index.html')
+
+def blogpost(request):
+    return render(request , 'main/blogpost.html')
+
+def contact(request):
+    return render(request , 'main/contact.html')
